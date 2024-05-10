ed8e64a169767024476c6d8e77138c61d532c524
Nguyen Huong Giang <107170492+Giang163@users.noreply.github.com>
2024-05-11 01:14:01 +0700

    Update Customer order.sql

--- SQL_Project_Customer_orders/Customer order.sql
+++ SQL_Project_Customer_orders/Customer order.sql
@@ -1,18 +1,75 @@
-﻿select * from [dbo].[MANAGER]
+select * from [dbo].[MANAGER]
 select * from [dbo].[ORDERS]
 select * from [dbo].[Profiles]
 select * from [dbo].[Returns]
---1. How many orders have an order priority of 'Low'?
-select count(distinct order_id) 
-from [dbo].[ORDERS]
 
---2. Top 5 provinces with the highest number of orders. 
-select top 5 [province], count(distinct order_id) 
+--The number of orders according to order_priority.
+select order_priority, count(distinct order_id) as num_of_orders
+from [dbo].[ORDERS]
+group by order_priority
+
+----ANALYZE ACCORDING TO GEOGRAGPHIC LOCATION----
+
+--Top 5 provinces with the highest number of orders. 
+select top 5 [province], count(distinct order_id) as num_of_order
 from [dbo].[ORDERS]
 group by province
-order by province desc
+order by num_of_order desc
 
---3. The top 3 managers with the highest number of orders.
+--The list of the top 5 products (product_name) with the highest total_value per region 
+select * from 
+(select (product_name),(region), sum([value]) total_value,
+ROW_NUMBER () over ( partition by [region] order by sum([value]) desc) as product_rank
+from [dbo].[ORDERS]
+group by [product_name],[region]) T
+where  product_rank < 6
+
+------The list of products (product_name) with the lowest ranked total_value per province.
+
+select * from 
+(select (product_name),(province), sum([value]) total_value,
+dense_rank () over ( partition by province order by sum([value]) ) as xephang
+from [dbo].[ORDERS]
+group by [product_name],province) T
+where  xephang =1
+
+
+---- CUSTOMER ANALYSIS---------
+
+---- Total value  for each customer segment.
+select customer_segment, sum(value) as total_revenue,  ((sum(value) )/(select sum(value) from [dbo].[ORDERS]))*100 as 'percentage of the total (%)'
+from [dbo].[ORDERS]
+group by customer_segment
+order by total_revenue desc
+
+------ Top 10 customer with highest value 
+select top 10 customer_name, sum(value) as total_revenue 
+from [dbo].[ORDERS]
+group by customer_name
+order by total_revenue desc 
+
+
+
+---- PRODUCT ANALYSIS---
+
+----The list of Product Categories with an average profit higher than the average profit of all orders
+select * from
+(
+select [Product_category], sum(profit) sum_profit 
+from [dbo].[ORDERS]
+group by [Product_category] 
+) T
+where sum_profit > (select  avg(profit)
+						from [dbo].[ORDERS]
+				)
+
+----- The order quantity each product by time .----
+select product_name, month(order_date) as month, year(order_date) as year , sum(order_quantity) as sum_quantity 
+from [dbo].[ORDERS]
+group by month(order_date), year(order_date), product_name
+order by product_name, year 
+
+-----The top 3 managers with the highest number of orders.
 select top 3 b.manager_name, count(distinct order_id) donhang from
 (
 (select [province], [order_id] from [dbo].[ORDERS] ) A
@@ -27,36 +84,9 @@ group by b.manager_name
 
 order by donhang desc
 
------4. The list of Product Categories with an average profit higher than the average profit of all orders
-select * from
-(
-select [Product_category], sum(profit) tongloinhuan
-from [dbo].[ORDERS]
-group by [Product_category] 
-) T
-where [tongloinhuan] > (select  avg(profit)
-						from [dbo].[ORDERS]
-				)
 
---5. The list of the top 5 products (product_name) with the highest total_value per region 
-select * from 
-(select (product_name),(region), sum([value]) total_value,
-ROW_NUMBER () over ( partition by [region] order by sum([value]) desc) as xephang
-from [dbo].[ORDERS]
-group by [product_name],[region]) T
-where  xephang < 6
-
-------6.The list of products (product_name) with the lowest ranked total_value per province.
-
-select * from 
-(select (product_name),(province), sum([value]) total_value,
-dense_rank () over ( partition by province order by sum([value]) ) as xephang
-from [dbo].[ORDERS]
-group by [product_name],province) T
-where  xephang =1
-
----7. table with columns year, month, total_revenue, total_revenue_returned,  acc_revenue, group_revenue
-create table [dbo]. [cuoikhoa]
+---Table with columns year, month, total_revenue, total_revenue_returned,  acc_revenue, group_revenue
+create table [dbo]. [final]
 			([year] nvarchar(4)
 			, [month] nvarchar(3)
 			, total_revenue float
@@ -64,9 +94,9 @@ create table [dbo]. [cuoikhoa]
 			, acc_revenue float
 			, group_revenue float
 				)
-create table [dbo]. [cuoikhoa1]
+create table [dbo]. [final1]
 
-select * into [cuoikhoa1] from [dbo].[ORDERS]
+select * into [final] from [dbo].[ORDERS]
 
 select 
 (select A.*, B.[status] from
@@ -75,9 +105,9 @@ full join
 (select [order_id],[status] from [dbo].[Returns] ) B
 on A.[order_id] = B.[order_id]) 
 
-into [cuoikhoa3] from [dbo].[ORDERS]
+into [final3] from [dbo].[ORDERS]
 
-INSERT INTO [cuoikhoa2]
+INSERT INTO [final2]
 
 select A.*, B.[status] from
 (select * from [dbo].[ORDERS] ) A
@@ -85,7 +115,7 @@ full join
 (select [order_id],[status] from [dbo].[Returns] ) B
 on A.[order_id] = B.[order_id]
 
-INSERT INTO [dbo]. [cuoikhoa] ([year], [month], [total_revenue], [total_revenue_returned],  [acc_revenue], [group_revenue])
+INSERT INTO [dbo]. [final] ([year], [month], [total_revenue], [total_revenue_returned],  [acc_revenue], [group_revenue])
 SELECT year(order_date)
 	,month(order_date)
 	,order_quantity* unit_price*(1-discount)
